59d16c87d3e6a10e93f261ca35e77abd11a6ee9e
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2025-10-08 19:28:41 +0800

    Fix Parts PDF to updated StorefrontAPI token

--- assets/custom-storefront.js
+++ assets/custom-storefront.js
@@ -18,7 +18,6 @@ let contactSection = `<!-- Contact Section -->
 </section>`;
 
 const locations = [
-  // ...existing locations array...
   {
     name: "Calgary",
     src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.039268029768!2d-113.9721757!3d50.9969465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53717a3ff80d1c5b%3A0x52d8421036f8f931!2sBacktrack%20Railway%20Services%20Ltd.!5e0!3m2!1sen!2sph!4v1742805783319!5m2!1sen!2sph",
---
+++ assets/partspdf.js
@@ -0,0 +1,54 @@
+console.log('hello from partspdf.js');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(product.id);
+    async function fetchProductMetafields() {
+        const client = ShopifyStorefrontAPIClient.createStorefrontApiClient({
+              storeDomain: 'http://backtrack-7a34.myshopify.com',
+              apiVersion: '2025-10',
+              publicAccessToken: '8d1afb3ac67f61264f4ee3bb73fe3956'
+        });
+
+        const shopQuery = `
+          query {
+            product(id: "gid://shopify/Product/${product.id}") {
+              partsPdf: metafield(namespace: "custom", key: "parts_pdf") {
+                id
+                namespace
+                key
+                value
+                type
+              }
+              pagePdfFile: metafield(namespace: "custom", key: "page_pdf_file") {
+                id
+                namespace
+                key
+                value
+                type
+              }
+            }
+          }
+          `;
+
+        const {data, errors, extensions} = await client.request(shopQuery);
+        console.log('test', data)
+        console.log('errors', errors)
+
+        let fileURL = data.product.partsPdf.value;
+        
+        if(fileURL != null) {
+          let baseElement = document.querySelector('.product-form--atc')
+          let button = `<div class="atc--button" style="width:100%: padding: 0; margin-top: 20px">
+                <a href="${fileURL}" target="_blank">
+                  <button class="see-the-price--btn productitem--action-atc button-primary product-form--atc-button mdc-ripple-surface mdc-ripple-upgraded" type="button">
+                    <span class="atc-button--text">Download PDF</span>
+                  </button>
+                </a>
+              </div>`;
+
+          baseElement.insertAdjacentHTML('afterend', button);
+        }
+    }
+
+    fetchProductMetafields();
+
+  }); 
